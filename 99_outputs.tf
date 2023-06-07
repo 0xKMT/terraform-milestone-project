2547ba58e54368d321729e2f80321596948d6ccb
Tri Pham Kha Minh <pkmtri@tma.com.vn>
2023-06-07 19:42:02 +0700

    add outputs

--- 99_outputs.tf
+++ 99_outputs.tf
@@ -17,3 +17,13 @@ output "internet_gateway_id" {
   description = "ID of internet gateway"
   value       = "${aws_internet_gateway.internet_gateway.*.id}"
 }
+
+output "public route table" {
+  description = "ID of public route table"
+  value       = "${aws_route_table.public_route_table.*.id}"
+}
+
+output "internet_gateway_id" {
+  description = "ID of internet gateway"
+  value       = "${aws_internet_gateway.internet_gateway.*.id}"
+}
